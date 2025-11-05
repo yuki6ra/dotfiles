92324b00c94b1d9bfa20ee4d051ffa6e232cb88e
yuki6ra <111046707+yuki6ra@users.noreply.github.com>
2025-11-05 14:10:14 +0900

    update: 補完

--- zsh/sync/path.zsh
+++ zsh/sync/path.zsh
@@ -13,3 +13,8 @@ eval "$(mise activate zsh)"
 
 # mise
 export PATH="$HOME/.local/share/mise/shims:$PATH"
+
+# 補完
+autoload -U +X bashcompinit && bashcompinit
+complete -o nospace -C /opt/homebrew/bin/terraform terraform
+source $(brew --prefix)/etc/bash_completion.d/az

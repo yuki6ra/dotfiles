2b4efc332267d041f6c22be535d74c9bb6e35fa0
yuki6ra <111046707+yuki6ra@users.noreply.github.com>
2026-02-13 14:50:43 +0900

    nix-darwinを追加

--- flake.nix
+++ flake.nix
@@ -8,14 +8,23 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     neovim = {
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    { nixpkgs, home-manager, neovim, ... } @inputs :
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nix-darwin,
+    neovim,
+    ... } @inputs :
     let
       overlays = [
         neovim.overlays.default
@@ -26,24 +35,59 @@
     in
     {
       homeConfigurations =
-        import ./nix/home-manager/.profile
-        |> builtins.mapAttrs(_name: { user, modules, system }:
-           let
-              pkgs = nixpkgs.legacyPackages.${system};
-            in
-            home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules =
-              modules
-              ++ [
-                {
-                  nixpkgs.overlays = overlays;
-                }
-              ];
-            extraSpecialArgs = specialArgsBase // {
-              inherit user system;
-            };
-          }
-        );
+        builtins.mapAttrs(_name: { user, modules, system }:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules =
+            modules
+            ++ [
+              {
+                nixpkgs.overlays = overlays;
+              }
+            ];
+          extraSpecialArgs = specialArgsBase // {
+            inherit user system;
+          };
+        }
+      )(import ./nix/home-manager/.profile);
+
+      darwinConfigurations = 
+      let
+        system = "aarch64-darwin";
+        # darwinSystemArgs = 
+        # {
+        #   profile,
+        #   username,
+        #   system,
+        # };
+        # import ./nix/nix-darwin {
+        #   inherit
+        #     inputs
+        #     profile
+        #     username
+        #     system
+        #     ;
+        # };
+      in
+      {
+        "mymac" = nix-darwin.lib.darwinSystem {
+          system = system;
+          modules = [
+            ./nix/nix-darwin/configuration.nix
+            home-manager.darwinModules.home-manager
+          ];
+          specialArgs = {
+            inherit inputs;
+          };
+        # asu = nix-darwin.lib.darwinSystem (darwinSystemArgs {
+        #     profile = "asu";
+        #     username = "nanami";
+        #     system = "aarch64-darwin";
+        #   });
+        };
+      };
     };
 }
--- nix/nix-darwin/configuration.nix
+++ nix/nix-darwin/configuration.nix
@@ -5,7 +5,6 @@
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
-    "pipe-operators"
   ];
 
   system = {
@@ -13,6 +12,23 @@
     # mac のユーザー名
     # `whoami` で確認可能
     primaryUser = "yuki6ra";
+    defaults = {
+      finder = {
+        AppleShowAllExtensions = true;  # ファイル拡張子を表示
+        AppleShowAllFiles = true;    # 隠しファイルを表示
+        FXEnableExtensionChangeWarning = false; # 拡張子変更の警告を無効化
+        ShowPathbar = true;  # パスバーを表示
+        ShowStatusBar = true;  # ステータスバーを表示
+      };
+      dock = {
+        autohide = true;  # Dockを自動的に隠す
+        show-recents = false; # 最近使ったアプリを非表示
+        tilesize = 36; # Dockのアイコンサイズ
+        magnification = true; # Dockの拡大表示を有効化
+        orientation = "bottom"; # Dockの位置(bottom / left / right)
+        launchanim = false; # アプリ起動時のアニメーションを無効化
+      };
+    };
   };
 
   nixpkgs = {
@@ -24,16 +40,55 @@
     enable = true;
     onActivation.cleanup = "none";
     # `brew tap` で確認可能
-    taps = [ ];
+    taps = [
+      "hashicorp/tap"
+      "xwmx/taps"
+    ];
     # `brew list --formula` で確認可能
     brews = [
+      "awscli"
+      "graphite2"
+      "ripgrep"
+      "azure-cli"
+      "harfbuzz"
+      "mise"
+      "nb"
+      "sheldon"
+      "terraform"
+      "colima"
+      "docker"
+      "tree"
+      "bash-completion@2"
+      "tree-sitter-cli"
+      "ffmpeg"
+      "openssl@3"
+      "fzf"
+      "lima"
+      "yazi"
       "git"
       "gnu-time"
+      "lua"
+      "zoxide"
+      "lua-language-server"
+      "presenterm"
     ];
     # `brew list --cask` で確認可能
     casks = [
-      "claude"
+      "alt-tab"
+      "discord"
+      "google-chrome"
+      "obsidian"
+      "raycast"
+      "arc"
+      "font-cica"
+      "microsoft-edge"
+      "orbstack"
       "visual-studio-code"
+      "claude"
+      "ghostty"
+      "notion"
+      "powershell"
+      "wezterm@nightly"
     ];
   };
 }

51f4392187ef308a3edcea27b68e2511ebd3d9b6
yuki6ra <111046707+yuki6ra@users.noreply.github.com>
2026-09-21 19:58:22 +0900

    miseでcli管理するこころみみ

--- .config/zsh/sync/config.zsh
+++ .config/zsh/sync/config.zsh
@@ -7,5 +7,3 @@ setopt hist_ignore_all_dups # 重複を全て削除
 setopt share_history        # 同時に起動しているzshで履歴を共有する
 setopt inc_append_history   # 即時追記（share_history と併用）
 
-# nix
-# . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
--- .config/zsh/sync/path.zsh
+++ .config/zsh/sync/path.zsh
@@ -1,15 +1,2 @@
-# homebrew
-# 社用はbrewパス生成をコメントアウト
-# eval "$(/opt/homebrew/bin/brew shellenv)"
+export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
 
-# mise
-# export PATH="$HOME/.local/share/mise/shims:$PATH"
-
-# 補完
-# source $(brew --prefix)/etc/bash_completion.d/az
-
-# export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
-
-export PATH="$PATH:/opt/homebrew/bin"
-
-# export PATH="$HOME/.local/bin:$PATH"
